1d277da6ca54d2173ad926223402a07ee9f8d18b
SergiiSurin <sergiisurin@gmail.com>
2023-05-31 09:50:58 +0300

    First init

---
+++ Ravesli_2/Ravesli_2/Ravesli_2.cpp
@@ -0,0 +1,45 @@
+﻿/* Ravesli_2.cpp : write a program that determines the minimum number 
+in a sequence of positive numbers that the user has entered. If there 
+are negative numbers in the sequence, then you must inform the user 
+about this and offer to repeat the input again.*/
+
+#include <iostream>
+#include "limits.h"
+
+bool enterOneMoreNumber()
+{
+    char ch;
+    do
+    {
+        std::cout << "Would you like to enter one more number (y/n)? ";
+        std::cin >> ch;
+    } while (ch != 'y' && ch != 'n');
+
+    return (ch == 'y');
+}
+
+int main()
+{
+    int num = 0;
+    int min = INT_MAX;
+   
+    std::cout << "Enter any sequence of positive numbers. " << std::endl;
+    
+    while (true)
+    { 
+        std::cin >> num;
+        if (num < 0)
+        {
+            std::cout << "You entered a negative number. Please enter a correct positive number " << std::endl;
+            continue;
+        }
+
+    if (num < min)
+        min = num;
+
+    if (!enterOneMoreNumber())
+        break;
+    
+    }
+    std::cout << "The minimal number in sequence is " << min << std::endl;
+}
---
+++ Ravesli_3/Ravesli_3_/Ravesli_3_.cpp
@@ -0,0 +1,39 @@
+﻿// Ravesli_3_.cpp : An ice cream sandwich is a string formed by two identical ends and
+// different middle. An ice cream sandwich must have a minimum length of three
+// character, and at least two of those characters must be different.
+
+#include <iostream>
+#include <string>
+
+bool isIcecreamSandwich(std::string str, int len);
+
+int main()
+{
+    std::string sequence;
+    std::cout << "Enter a sequence of two characters longer than three: ";
+    std::cin >> sequence;
+    
+    int len = sequence.length();
+        
+   std::cout << std::endl << "The " << sequence << " is" << ((isIcecreamSandwich(sequence, len)) ? " " : " not ") << "an Icecream Sandwich" << std::endl;
+   
+}
+
+bool isIcecreamSandwich(std::string str, int len)
+{
+    if (len < 3)
+        return false;
+    int count = 1;
+    char tmp = str[0];
+    std::string reverseStr = str;
+    for (int i = 0, j = len-1; i < len; ++i, --j)
+    {
+        if (tmp != str[i] && i<=(len/2))
+            ++count;
+        reverseStr[j] = str[i];
+        tmp = str[i];
+    }
+    if (count != 2)
+        return false;
+    return (str == reverseStr);
+}
